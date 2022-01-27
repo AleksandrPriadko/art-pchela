d8ff37ab437a5eb0827c0ea3e1100a03d85f3f8e
AleksandrPriadko <68820567+AleksandrPriadko@users.noreply.github.com>
2022-01-28 00:22:34 +0200

    add click next or prev img

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,19 +1,64 @@
-import portfolio from '../portfolio.json';
+const sliderOne = document.querySelector('.block-portfolio__list-one');
+const sliderTwo = document.querySelector('.block-portfolio__list-two');
+console.log('Slider: ', sliderOne);
 
-console.log(portfolio);
+const prevBtn = document.querySelector('.btn-slider-prev');
+const nextBtn = document.querySelector('.btn-slider-next');
 
-const blockPortfolio = document.querySelector('.block-portfolio');
-console.log(blockPortfolio);
+let size = document.querySelector('.block-portfolio__item'); //.clientWidth;
+console.dir(size);
+let direction = 0;
+let width = 652;
+// buttons
+nextBtn.addEventListener('click', onClickImgNext);
+prevBtn.addEventListener('click', onClickImgPrev);
 
-portfolio.map(({ id, imgUrl, alt }) => {
-  const markup = `<ul class="block-portfolio__list-one"><li class="block-portfolio__item">
-              <picture>
-                <img
-                  src="${imgUrl}"
-                  alt="${alt}"
-                  class="block-portfolio__link"
-                />
-              </picture>
-            </li></ul>`;
-  blockPortfolio.insertAdjacentHTML('beforeend', markup);
-});
+function onClickImgNext() {
+  //console.log((direction += 1));
+  direction += 1;
+  sliderOne.style.transform = `translateX(${-574 - width * direction}px)`;
+  sliderTwo.style.transform = `translateX(${401.5 - width * direction}px)`;
+  // console.log(
+  //   (sliderTwo.style.transform = `translateX(${401.5 - width * direction}px)`),
+  // );
+}
+function onClickImgPrev() {
+  //console.log((direction -= 1));
+  direction -= 1;
+  sliderOne.style.transform = `translateX(${-574 - width * direction}px)`;
+  sliderTwo.style.transform = `translateX(${401.5 - width * direction}px)`;
+  // console.log(
+  //   (sliderTwo.style.transform = `translateX(${401.5 - width * direction}px)`),
+  // );
+}
+// nextBtn.onclick = function () {
+//   slider.style.transform = 'translateX(' + -size + 'px)';
+//   direction = 1;
+// };
+
+// prevBtn.onclick = function () {
+//   direction = -1;
+//   slider.style.transform = 'translateX(' + size + 'px)';
+// };
+
+// after transition event
+// slider.addEventListener('transitionend', function () {
+//   let slides = document.querySelectorAll('.block-portfolio__item');
+
+//   if (direction < 0) {
+//     for (let i = 0; i > direction; i--) {
+//       slider.prepend(slides[slides.length + i - 1]);
+//     }
+//   } else if (direction > 0) {
+//     for (let i = 0; i < direction; i++) {
+//       slider.append(slides[i]);
+//     }
+//   }
+
+//   // re-init transition
+//   slider.style.transition = 'none';
+//   slider.style.transform = 'translate(0)';
+//   setTimeout(() => {
+//     slider.style.transition = 'transform 0.5s ease-in-out';
+//   }, 0);
+// });
