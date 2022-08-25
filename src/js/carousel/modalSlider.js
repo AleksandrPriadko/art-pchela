ad5200a033dde0477f085c97045ed6a19ff1a90f
AleksandrPriadko <68820567+AleksandrPriadko@users.noreply.github.com>
2022-08-25 21:09:59 +0300

    fix open current picture

--- src/js/carousel/modalSlider.js
+++ src/js/carousel/modalSlider.js
@@ -1,29 +1,20 @@
 import imgArray from '../../portfolio.json';
 const modalSlider = document.querySelector('.backdrop-slider');
 const listImg = document.querySelector('.block-portfolio__list-one');
+
 const listSlider = document.querySelector('.list-slider');
 
 const closeBtn = document.querySelector('.btn-close');
 const btnNextImg = document.querySelector('.js-btn-next-img');
 const btnPrevImg = document.querySelector('.js-btn-prev-img');
 
-const newArr = imgArray;
- const itemsGalleryArr = newArr.map(image => {
-    const item = document.createElement('li');
-    item.classList.add('item-gallery');
-    const img = document.createElement('img');
-    img.setAttribute('src', image.smallImg);
-    img.classList.add('img-slider');
-    item.append(img);
-    return item;
- });
+const item = document.createElement('li');
+item.classList.add('item-gallery');
+const img = document.createElement('img');
+img.classList.add('img-slider');
 
- itemsGalleryArr.map(item=> console.log(item))
-  
 listImg.addEventListener('click', handleImgClick);
-closeBtn.addEventListener('click', () =>
-  document.body.classList.remove('show-modal-slider'),
-);
+closeBtn.addEventListener('click', closeSlider);
 
 function handleImgClick(event) {
   event.preventDefault();
@@ -32,31 +23,18 @@ function handleImgClick(event) {
     return;
   }
   const largeImgUrl = target.src;
-  console.log(largeImgUrl)
-  const find = newArr.find(url => 
-    url.smallImg === largeImgUrl
-  );
-  console.log(find)
-  let indexImg = newArr.indexOf(find);
-  console.log(indexImg)
-  //addItemsGalleryInSlide(indexImg)
-  currentImage(indexImg);
+  const find = imgArray.find(url => url.smallImg === largeImgUrl);
+  let indexImg = imgArray.indexOf(find);
+
+  img.setAttribute('src', imgArray[indexImg].smallImg);
+  item.append(img);
+  listSlider.append(item);
+
   return document.body.classList.add('show-modal-slider');
 }
+function closeSlider() {
+  item.remove();
+  document.body.classList.remove('show-modal-slider');
 
-function currentImage(indexImg) {
-  console.log(indexImg)
- 
-  const delArr = itemsGalleryArr.splice(indexImg, 1);
-  itemsGalleryArr.unshift(delArr[0])
-  
-  return listSlider.append(...itemsGalleryArr);
+  return;
 }
-//modalImg.appendChild(closeBtn);
-//modalSlider.append(modalImg);
-// function addItemsGalleryInSlide(indexImg) {
-  
-//   const delArr = itemsGalleryArr.splice(indexImg, 1);
-//   itemsGalleryArr.unshift(delArr[0])
-//  listSlider.append(...itemsGalleryArr);
-// }
